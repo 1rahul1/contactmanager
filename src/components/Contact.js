8aef9b23a8e1c05a297e93776575600e1adf63f4
Rahul Prasad <1rahulprasad1@gmail.com>
2020-01-29 23:20:26 +0530

    passing parameters in event

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 class Contact extends Component {
   state = {};
 
-  onShowClick = () => {
-    console.log(this.state);
+  onShowClick = (name, e) => {
+    console.log(name);
   };
 
   render() {
@@ -14,7 +14,10 @@ class Contact extends Component {
       <div className="card card-body mb-3">
         <h4>
           {contact.name}{' '}
-          <i onClick={this.onShowClick} className="fas fa-sort-down"></i>
+          <i
+            onClick={this.onShowClick.bind(this, contact.name)}
+            className="fas fa-sort-down"
+          ></i>
         </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {contact.email}</li>
