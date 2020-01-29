87343802c98a2870a6c89fc0092ca3c1d6015c35
Rahul Prasad <1rahulprasad1@gmail.com>
2020-01-29 22:49:56 +0530

    remove constructor in Contacts component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,31 +2,28 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'John Doe',
-          email: 'jdoe@gmail.com',
-          phone: '555-555-5555'
-        },
-        {
-          id: 2,
-          name: 'Karen Doe',
-          email: 'jdoe@gmail.com',
-          phone: '555-555-5555'
-        },
-        {
-          id: 3,
-          name: 'Lucy Doe',
-          email: 'jdoe@gmail.com',
-          phone: '555-555-5555'
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 2,
+        name: 'Karen Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 3,
+        name: 'Lucy Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
+      }
+    ]
+  };
 
   render() {
     const { contacts } = this.state;
