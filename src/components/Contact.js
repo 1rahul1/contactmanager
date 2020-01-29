125fe186ec3397014380820676567bc7f0ec64e2
Rahul Prasad <1rahulprasad1@gmail.com>
2020-01-29 23:24:07 +0530

    replace div in contact with React.Fragment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,7 @@ import PropTypes from 'prop-types';
 class Contact extends Component {
   state = {};
 
-  onShowClick = (name, e) => {
-    console.log(name);
-  };
+  onShowClick = e => {};
 
   render() {
     const { contact } = this.props;
@@ -14,10 +12,7 @@ class Contact extends Component {
       <div className="card card-body mb-3">
         <h4>
           {contact.name}{' '}
-          <i
-            onClick={this.onShowClick.bind(this, contact.name)}
-            className="fas fa-sort-down"
-          ></i>
+          <i onClick={this.onShowClick} className="fas fa-sort-down"></i>
         </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {contact.email}</li>
