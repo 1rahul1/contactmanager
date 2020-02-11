ed9d43cb13127caf534fb9f50dae282cb40cc23f
Rahul Prasad <1rahulprasad1@gmail.com>
2020-02-11 19:33:04 +0530

    add error checking and display

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const TextInputGroup = ({
   label,
@@ -7,19 +8,23 @@ const TextInputGroup = ({
   value,
   placeholder,
   type,
-  onChange
+  onChange,
+  error
 }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input
         type={type}
-        className="form-control form-control-lg"
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
+        })}
         placeholder={placeholder}
         name={name}
         value={value}
         onChange={onChange}
       />
+      {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
@@ -30,7 +35,8 @@ TextInputGroup.prototypes = {
   value: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  error: PropTypes.string.isRequired
 };
 
 TextInputGroup.defaultProps = {
