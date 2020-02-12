fa41129f9d1a6ac9abca484f18b6527ea7e90907
Rahul Prasad <1rahulprasad1@gmail.com>
2020-02-12 15:07:38 +0530

    add a test component(temporary)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/layout/Header';
 import About from './components/pages/About';
 import AddContact from './components/contacts/AddContact';
 import NotFound from './components/pages/NotFound';
+import Test from './components/test/Test';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Provider } from './context';
@@ -23,6 +24,7 @@ class App extends Component {
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
               </Switch>
             </div>
