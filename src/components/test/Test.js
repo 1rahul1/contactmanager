1101e37bfbf459a2e45c966fea00f73b402d2312
Rahul Prasad <1rahulprasad1@gmail.com>
2020-02-12 16:48:19 +0530

    get request using axios

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -6,48 +6,9 @@ class Test extends Component {
   };
 
   componentDidMount() {
-    console.log(
-      'Component did mount - This is where you make http call to backend'
-    );
-  }
-
-  componentWillMount() {
-    console.log('Component will mount - Runs before ComponentsDidMount');
-  }
-
-  componentDidUpdate() {
-    console.log(
-      'Components Did Update - after components did update i.e on change of state'
-    );
-  }
-
-  componentWillUpdate() {
-    console.log('ComponentsWillUpdate...');
-  }
-
-  componentWillReceiveProps(nextProp, nextState) {
-    console.log(
-      'when your components receive new properties then this is run--------usually used with redux'
-    );
-  }
-
-  //componentWill* are getting depricated
-
-  static getDerivedStateFromProps(nextProp, nextState) {
-    return null;
-  }
-
-  getSnapshotBeforeUpdate(nextProp, nextState) {
-    console.log('getSnapshotBeforeUpdate...');
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Test Component</h1>
-      </div>
-    );
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => console.log(data));
   }
 }
-
 export default Test;
