7344ff8399d104f41e4e5076af434b52da831426
Rahul Prasad <1rahulprasad1@gmail.com>
2020-02-12 17:10:26 +0530

    add get, delete and post using axios

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
-import uuid from 'uuid';
 import TextInputGroup from '../layout/TextInputGroup';
+import axios from 'axios';
 
 class AddContact extends Component {
   state = {
@@ -30,13 +30,14 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
       name: name,
       email: email,
       phone: phone
     };
 
-    dispatch({ type: 'ADD_CONTACT', payload: newContact });
+    axios
+      .post('https://jsonplaceholder.typicode.com/users', newContact)
+      .then(res => dispatch({ type: 'ADD_CONTACT', payload: res.data }));
 
     //Clear input boxes
     this.setState({
