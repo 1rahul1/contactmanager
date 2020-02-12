35493115e9915f68ac7ebf34db19731635cafa8f
Rahul Prasad <1rahulprasad1@gmail.com>
2020-02-12 16:30:12 +0530

    test some lifecycle methods

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,6 +1,46 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
+  state = {
+    test: 'test'
+  };
+
+  componentDidMount() {
+    console.log(
+      'Component did mount - This is where you make http call to backend'
+    );
+  }
+
+  componentWillMount() {
+    console.log('Component will mount - Runs before ComponentsDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log(
+      'Components Did Update - after components did update i.e on change of state'
+    );
+  }
+
+  componentWillUpdate() {
+    console.log('ComponentsWillUpdate...');
+  }
+
+  componentWillReceiveProps(nextProp, nextState) {
+    console.log(
+      'when your components receive new properties then this is run--------usually used with redux'
+    );
+  }
+
+  //componentWill* are getting depricated
+
+  static getDerivedStateFromProps(nextProp, nextState) {
+    return null;
+  }
+
+  getSnapshotBeforeUpdate(nextProp, nextState) {
+    console.log('getSnapshotBeforeUpdate...');
+  }
+
   render() {
     return (
       <div>
