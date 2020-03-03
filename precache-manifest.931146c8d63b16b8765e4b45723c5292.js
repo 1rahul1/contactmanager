bb477bb23f80f2e9307e6c1f852151571b94271a
Rahul Prasad <1rahulprasad1@gmail.com>
2020-03-03 16:14:16 +0530

    Updates

---
+++ precache-manifest.931146c8d63b16b8765e4b45723c5292.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d527f2c497e1fde5e8b5e51b6931ae",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "b52043d535b0772a5585",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2b9fd89b8680370df423",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b52043d535b0772a5585",
+    "url": "/contactmanager/static/js/2.93f90149.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.93f90149.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b9fd89b8680370df423",
+    "url": "/contactmanager/static/js/main.78d747da.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
