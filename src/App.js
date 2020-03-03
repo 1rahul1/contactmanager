27c7ead871ed95a3191404c269a263cef0bd3ccd
Rahul Prasad <1rahulprasad1@gmail.com>
2020-03-03 15:50:56 +0530

    add EditContact component and fetch contact in form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 import NotFound from './components/pages/NotFound';
 import Test from './components/test/Test';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -22,6 +23,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/test" component={Test} />
